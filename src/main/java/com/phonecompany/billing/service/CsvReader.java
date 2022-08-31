5a017ddbaddf5ae7a11cec67c7ca0592dd96bc01
David <david.matyas.cz@gmail.com>
2022-08-31 11:56:33 +0200

    parse CSV into values

--- src/main/java/com/phonecompany/billing/service/CsvReader.java
+++ src/main/java/com/phonecompany/billing/service/CsvReader.java
@@ -5,10 +5,13 @@ import java.math.BigDecimal;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class CsvReader {
     public static void main(String[] args) {
@@ -26,15 +29,34 @@ public class CsvReader {
                 System.out.println("TEST" + Arrays.toString(values.get(i)));
                 for (int j = 0; j < 3; j++) {
                     System.out.println(values.get(i)[j]);
-                    System.out.println("Phone number " + values.get(i)[0] + " start call " + values.get(i)[1]  + "end" +
+                    System.out.println("Phone number " + values.get(i)[0] + " start call " + values.get(i)[1]  + " " +
+                            "end" +
                             " " +
                             "call " + values.get(i)[2]);
+                    SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+                    Date startDate = sdf.parse(values.get(i)[1]);
+                    Date endDate = sdf.parse(values.get(i)[2]);
+
+                    long diffInMillies = Math.abs(startDate.getTime() - endDate.getTime());
+                    long diff = TimeUnit.MINUTES.convert(diffInMillies, TimeUnit.MILLISECONDS);
+                    System.out.println(diff);
+
+//                    calculate(8,16);
+
                 }
             }
             System.out.println(lines.get(0));
         } catch (IOException e) {
             System.out.println("File is incorrect!");
+        } catch (ParseException e) {
+            e.printStackTrace();
         }
+    }
+
+    public static int calculate(int startHour, int endHour)
+    {
+//        if(startTime>0 )
+        return 1;
     }
 }
 
