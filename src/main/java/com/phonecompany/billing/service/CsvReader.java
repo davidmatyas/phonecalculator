516fa2fb0cd46261fbf42176f17a888e17fb3778
David <david.matyas.cz@gmail.com>
2022-08-31 13:23:18 +0200

    calculation with hours

--- src/main/java/com/phonecompany/billing/service/CsvReader.java
+++ src/main/java/com/phonecompany/billing/service/CsvReader.java
@@ -1,16 +1,13 @@
 package com.phonecompany.billing.service;
 
 import java.io.IOException;
-import java.math.BigDecimal;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 public class CsvReader {
@@ -19,34 +16,37 @@ public class CsvReader {
         Path filePath = Paths.get("phonelog.csv");
         List<String> lines;
         List<String[]> values = new ArrayList<>();
+        GregorianCalendar gc = new GregorianCalendar();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
         try {
             lines = Files.readAllLines(filePath);
             for (int i = 0; i < lines.size(); i++) {
-                System.out.println(lines.get(i).split(","));
+//                System.out.println(lines.get(i).split(","));
                 values.add(lines.get(i).split(","));
             }
             for (int i = 0; i < values.size(); i++) {
-                System.out.println("TEST" + Arrays.toString(values.get(i)));
-                for (int j = 0; j < 3; j++) {
-                    System.out.println(values.get(i)[j]);
-                    System.out.println("Phone number " + values.get(i)[0] + " start call " + values.get(i)[1]  + " " +
-                            "end" +
-                            " " +
-                            "call " + values.get(i)[2]);
-                    SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
-                    Date startDate = sdf.parse(values.get(i)[1]);
-                    Date endDate = sdf.parse(values.get(i)[2]);
+//                System.out.println("TEST" + Arrays.toString(values.get(i)));
+                Date startDate = sdf.parse(values.get(i)[1]);
+                Date endDate = sdf.parse(values.get(i)[2]);
+                gc.setTime(startDate);
+                System.out.println("Start hour: "+gc.get(Calendar.HOUR_OF_DAY));
+                gc.setTime(endDate);
+                System.out.println("End hour: "+gc.get(Calendar.HOUR_OF_DAY));
 
-                    long diffInMillies = Math.abs(startDate.getTime() - endDate.getTime());
-                    long diff = TimeUnit.MINUTES.convert(diffInMillies, TimeUnit.MILLISECONDS);
-                    System.out.println(diff);
-                    calculate((int) diff);
-                    System.out.println("PRICE" + calculate(50));
-                    System.out.println(calculate(5));
+                long diffInMillies = Math.abs(startDate.getTime() - endDate.getTime());
+                long diff = TimeUnit.MINUTES.convert(diffInMillies, TimeUnit.MILLISECONDS);
+//                System.out.println("HOUR "+ TimeUnit.HOURS.convert(startDate.getTime(), TimeUnit.HOURS));
+//                System.out.println(startDate.getTime());
+               // System.out.println(TimeUnit.HOURS(10,TimeUnit.MILLISECONDS));
+//                System.out.println(diff);
+                System.out.println("Phone number " + values.get(i)[0] + " start call " + values.get(i)[1] + " " +
+                        "end " +
+                        "call " + values.get(i)[2] + " duration " + diff + " price " + calculate((int) diff, 8,16));
+//                    calculate((int) diff);
+//                    System.out.println("PRICE" + calculate(50));
+//                    System.out.println(calculate(5));
 
-                }
             }
-            System.out.println(lines.get(0));
         } catch (IOException e) {
             System.out.println("File is incorrect!");
         } catch (ParseException e) {
@@ -54,12 +54,12 @@ public class CsvReader {
         }
     }
 
-    public static double calculate(int duration)
-    {
+    public static double calculate(int duration, int startHour, int endHour) {
         double price = 0;
-        if(duration > 5){
-            price = 5 + (duration -5) * 0.2;
-        return price;
+
+        if (duration > 5) {
+            price = 5 + (duration - 5) * 0.2;
+            return price;
         }
         return duration;
     }
